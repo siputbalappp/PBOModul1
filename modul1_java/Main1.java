540406cb4dd9db3e5af6d4a51b9669f2c3e7f733
Zarif Afzal Ramadhan <106903883+siputbalappp@users.noreply.github.com>
2022-10-25 07:55:18 +0700

    Add files via upload

---
+++ modul1_java/Main1.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.main1;
+
+/**
+ *
+ * @author Zarif Afzal R
+ */
+public class Main1 {
+
+    public static void main(String[] args) {
+        Welcome W=new Welcome ();
+        W.display();
+    }
+}
---
+++ modul1_java/Main2.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.main2;
+
+/**
+ *
+ * @author Zarif Afzal R
+ */
+public class Main2{
+    
+    public static void main(String args[])
+    {
+    mahasiswa M=new mahasiswa(); 
+    M.inputData();
+    M.display ();
+}
+
+    
+}
+
+
+   
+    
